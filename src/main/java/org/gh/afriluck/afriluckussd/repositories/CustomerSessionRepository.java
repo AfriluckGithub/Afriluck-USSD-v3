766a192dc4c200e66a2c457690816d6cc134420d
steven@afriluck <steven@afriluck.com>
2024-07-02 19:50:19 +0000

    time update

--- src/main/java/org/gh/afriluck/afriluckussd/repositories/CustomerSessionRepository.java
+++ src/main/java/org/gh/afriluck/afriluckussd/repositories/CustomerSessionRepository.java
@@ -1,8 +1,14 @@
 package org.gh.afriluck.afriluckussd.repositories;
 
 import org.gh.afriluck.afriluckussd.entities.Session;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface CustomerSessionRepository extends CrudRepository<Session, Integer> {
