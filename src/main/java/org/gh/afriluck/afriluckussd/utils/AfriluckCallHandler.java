5be3e3dec631f07dc0e456748672326606d25563
steven@afriluck <steven@afriluck.com>
2024-08-14 22:44:46 +0000

    fix headerr

--- src/main/java/org/gh/afriluck/afriluckussd/utils/AfriluckCallHandler.java
+++ src/main/java/org/gh/afriluck/afriluckussd/utils/AfriluckCallHandler.java
@@ -1,7 +1,6 @@
 package org.gh.afriluck.afriluckussd.utils;
 
-import org.springframework.data.web.XmlBeamHttpMessageConverter;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
@@ -10,13 +9,16 @@ import org.springframework.web.client.RestClient;
 @Component
 public class AfriluckCallHandler {
 
+    @Value("${env.data.apikey}")
+    private String apiKey;
+
     public RestClient client() {
         return RestClient.builder()
                 .requestFactory(new HttpComponentsClientHttpRequestFactory())
                 .messageConverters(converters -> converters.add(new StringHttpMessageConverter()))
                 .baseUrl("https://app.afriluck.com")
                 .defaultHeader("Content-Type", "application/text")
-                .defaultHeader("x-afriluck-key", "dmp2clBMZzo0ZDlmZDI5NTY4MDNzRiZTI4YzZkMmRiMDE4ZTY1NmE0MTE5Y2M4Yg")
+                .defaultHeader("x-afriluck-key", apiKey)
                 .build();
     }
 }
