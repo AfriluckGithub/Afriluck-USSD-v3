bfbfa9839d9e23aee20c0c0b4aa6a7a4f5a9c6bb
steven@afriluck <steven@afriluck.com>
2024-08-14 22:01:39 +0000

    fix headerr

--- src/main/java/org/gh/afriluck/afriluckussd/utils/AfriluckCallHandler.java
+++ src/main/java/org/gh/afriluck/afriluckussd/utils/AfriluckCallHandler.java
@@ -1,6 +1,7 @@
 package org.gh.afriluck.afriluckussd.utils;
 
 import org.springframework.data.web.XmlBeamHttpMessageConverter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ public class AfriluckCallHandler {
                 .messageConverters(converters -> converters.add(new StringHttpMessageConverter()))
                 .baseUrl("https://app.afriluck.com")
                 .defaultHeader("Content-Type", "application/text")
+                .defaultHeader("x-afriluck-key", "dmp2clBMZzo0ZDlmZDI5NTY4MDNzRiZTI4YzZkMmRiMDE4ZTY1NmE0MTE5Y2M4Yg")
                 .build();
     }
 }
