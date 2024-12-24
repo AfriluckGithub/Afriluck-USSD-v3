a22698b2147dea893ff90a59b897eb2b9ca2c729
steven@afriluck <steven@afriluck.com>
2024-12-24 18:33:18 +0000

    fix

--- src/main/java/org/gh/afriluck/afriluckussd/controllers/UssdController.java
+++ src/main/java/org/gh/afriluck/afriluckussd/controllers/UssdController.java
@@ -124,7 +124,8 @@ public class UssdController {
                 }
                 if (s.getNextStep() == ZERO) {
                     s.setNextStep(FIRST);
-                    String dayOfWeekInWords = getDayOfWeekInWords();
+                    //String dayOfWeekInWords = getDayOfWeekInWords();
+                    String dayOfWeekInWords = "Thursday";
                     updateSession(s, false);
 
 
