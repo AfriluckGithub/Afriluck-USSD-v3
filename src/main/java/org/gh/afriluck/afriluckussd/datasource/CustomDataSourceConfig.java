ad06f819655b2ec64a9ac3b84adb1a4775966469
steven@afriluck <steven@afriluck.com>
2025-06-17 19:07:17 +0000

    fix

--- src/main/java/org/gh/afriluck/afriluckussd/datasource/CustomDataSourceConfig.java
+++ src/main/java/org/gh/afriluck/afriluckussd/datasource/CustomDataSourceConfig.java
@@ -14,7 +14,7 @@ public class CustomDataSourceConfig {
     static {
         HikariConfig config = new HikariConfig();
         try {
-            config.setJdbcUrl("jdbc:postgresql://10.180.180:5432/afriluck_sessions");
+            config.setJdbcUrl("jdbc:postgresql://91.107.192.27:19997/afriluck_sessions");
             config.setUsername("root");
             config.setPassword("root");
             config.setDriverClassName("org.postgresql.Driver");
