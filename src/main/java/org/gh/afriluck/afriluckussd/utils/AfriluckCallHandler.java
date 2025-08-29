2f181938e5f3897d577c08b0a2293ef7694480f2
steven@afriluck <steven@afriluck.com>
2025-08-29 14:04:38 +0000

    fix

--- src/main/java/org/gh/afriluck/afriluckussd/utils/AfriluckCallHandler.java
+++ src/main/java/org/gh/afriluck/afriluckussd/utils/AfriluckCallHandler.java
@@ -19,7 +19,8 @@ public class AfriluckCallHandler {
         return RestClient.builder()
                 .requestFactory(new HttpComponentsClientHttpRequestFactory())
                 .messageConverters(converters -> converters.add(new StringHttpMessageConverter()))
-                .baseUrl(baseUrl)
+                //.baseUrl(baseUrl)
+                .baseUrl("http://staging.afriluck.com:5050")
                 .defaultHeader("Content-Type", "application/text")
                 .defaultHeader("x-afriluck-key", apiKey)
                 .build();
