54f8188fc68f887170a0a44566fe43d825880a01
steven@afriluck <steven@afriluck.com>
2025-11-05 20:34:59 +0000

    fix

--- src/main/java/org/gh/afriluck/afriluckussd/dto/CustomerDepositResponseDto.java
+++ src/main/java/org/gh/afriluck/afriluckussd/dto/CustomerDepositResponseDto.java
@@ -3,11 +3,14 @@ package org.gh.afriluck.afriluckussd.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class CustomerDepositResponseDto {
 
     @JsonProperty("success")
